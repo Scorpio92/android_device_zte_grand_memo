199147437d87c38e048f1dd8067d9e7d878fd29b
scorpio92 <scorpio92@mail.ru>
2015-07-30 22:32:26 +1000

    vendor.mk update

--- vendor.mk
+++ vendor.mk
@@ -1,16 +1,3 @@
-#Auidio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/proprietary/bin/aplay:system/bin/aplay \
-    $(LOCAL_PATH)/proprietary/etc/snd_soc_msm/snd_soc_msm:system/etc/snd_soc_msm/snd_soc_msm \
-    $(LOCAL_PATH)/proprietary/etc/snd_soc_msm/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x \
-    $(LOCAL_PATH)/proprietary/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3:system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3 \
-    $(LOCAL_PATH)/proprietary/etc/snd_soc_msm/snd_soc_msm_Sitar:system/etc/snd_soc_msm/snd_soc_msm_Sitar \
-    $(LOCAL_PATH)/proprietary/etc/audio_effects.conf:system/etc/audio_effects.conf \
-    $(LOCAL_PATH)/proprietary/etc/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/proprietary/etc/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/proprietary/etc/media_profiles.xml:system/etc/media_profiles.xml \
-    $(LOCAL_PATH)/proprietary/lib/hw/audio.primary.msm8960.so:system/lib/hw/audio.primary.msm8960.so \
-    $(LOCAL_PATH)/proprietary/lib/hw/audio_policy.msm8960.so:system/lib/hw/audio_policy.msm8960.so \
-    $(LOCAL_PATH)/proprietary/lib/libalsa-intf.so:system/lib/libalsa-intf.so \
-    $(LOCAL_PATH)/proprietary/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf
+$(call find-copy-subdir-files,*,device/zte/grand_memo/proprietary,system)
 
